6fbb278174d240716c6971131529771b549c2b1d
WalterXiong <xiongjun-ol@outlook.com>
2024-12-19 22:31:47 +0800

    新增-- BroadcastReceiver 发送自定义标准广播

--- 6_BroadcastReceiver/BroadcastTest/app/src/main/java/com/androidlearning/broadcasttest/MainActivity.kt
+++ 6_BroadcastReceiver/BroadcastTest/app/src/main/java/com/androidlearning/broadcasttest/MainActivity.kt
@@ -3,7 +3,6 @@ package com.androidlearning.broadcasttest
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import android.content.IntentFilter
 import android.os.Bundle
 import android.widget.Button
 import android.widget.Toast
@@ -27,19 +26,25 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
-        val intentFilter = IntentFilter()
-        intentFilter.addAction("android.intent.action.TIME_TICK")
+        /**
+         * 动态注册广播
+         */
+        // val intentFilter = IntentFilter()
+        // intentFilter.addAction("android.intent.action.TIME_TICK")
+        // timeChangeReceiver = TimeChangeReceiver()
+        // registerReceiver(timeChangeReceiver, intentFilter)
 
-        timeChangeReceiver = TimeChangeReceiver()
 
-        registerReceiver(timeChangeReceiver, intentFilter)
-
-
-        //
+        /**
+         * 发送自定义广播
+         */
         val button = findViewById<Button>(R.id.button)
         button.setOnClickListener {
+
             val intent = Intent("com.walter.broadcasttest.MY_BROADCAST")
             intent.setPackage(packageName)
+
+            // 发送标准广播
             sendBroadcast(intent)
         }
 
