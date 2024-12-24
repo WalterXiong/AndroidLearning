c21ec4ab12684345df89cf3befcbfa169fef87fa
WalterXiong <xiongjun-ol@outlook.com>
2024-12-24 21:40:00 +0800

    新增-- BroadcastBestPractice 发送强制下线广播，完善记住密码

--- 6_BroadcastReceiver/BroadcastBestPractice/app/src/main/java/com/androidlearning/broadcastbestpractice/LoginActivity.kt
+++ 6_BroadcastReceiver/BroadcastBestPractice/app/src/main/java/com/androidlearning/broadcastbestpractice/LoginActivity.kt
@@ -1,9 +1,11 @@
 package com.androidlearning.broadcastbestpractice
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import android.widget.CheckBox
 import android.widget.EditText
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
@@ -20,23 +22,41 @@ class LoginActivity : BaseActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
-
         val accountEdit: EditText = findViewById(R.id.accountEdit)
         val passwordEdit: EditText = findViewById(R.id.passwordEdit)
+        val rememberPass: CheckBox = findViewById(R.id.rememberPass)
+
+        val sp = getSharedPreferences("remember_password", Context.MODE_PRIVATE)
+        val isRemember = sp.getBoolean("remember_password", false)
+        if (isRemember) {
+
+            val account = sp.getString("account", "")
+            val password = sp.getString("password", "")
+
+            accountEdit.setText(account)
+            passwordEdit.setText(password)
+
+            rememberPass.isChecked = true
+        }
 
 
         val loginButton: Button = findViewById(R.id.login)
         loginButton.setOnClickListener {
-
             val account = accountEdit.text.toString()
-            Log.d("LoginActivity", account)
-
             val password = passwordEdit.text.toString()
-            Log.d("LoginActivity", password)
-
             // 如果账号是admin且密码是123456，就认为登录成功
             if (account == "admin" && password == "123456") {
+
+                val editor = sp.edit()
+                if (rememberPass.isChecked) {
+                    editor.putString("account", account)
+                    editor.putString("password", password)
+                    editor.putBoolean("remember_password", true)
+                } else {
+                    editor.clear()
+                }
+                editor.apply()
+
                 val intent = Intent(this, MainActivity::class.java)
                 startActivity(intent)
                 finish()
