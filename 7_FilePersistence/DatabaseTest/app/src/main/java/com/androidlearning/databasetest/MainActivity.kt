ab5ab79e9ff5247a33dda3f96ffdf5c5cc7ca8dd
WalterXiong <xiongjun-ol@outlook.com>
2024-12-26 15:23:31 +0800

    新增-- DatabaseTest CRUD 操作

--- 7_FilePersistence/DatabaseTest/app/src/main/java/com/androidlearning/databasetest/MainActivity.kt
+++ 7_FilePersistence/DatabaseTest/app/src/main/java/com/androidlearning/databasetest/MainActivity.kt
@@ -1,13 +1,18 @@
 package com.androidlearning.databasetest
 
+import android.annotation.SuppressLint
+import android.content.ContentValues
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -20,15 +25,77 @@ class MainActivity : AppCompatActivity() {
 
         val createDatabase: Button = findViewById(R.id.createDatabase)
 
+        // 创建数据库
         // val myDatabaseHelper = MyDatabaseHelper(this, "BookStore.db", 1)
 
         // 升级数据库
-        val myDatabaseHelper = MyDatabaseHelper(this, "BookStore.db", 2)
-
+        val dbHelper = MyDatabaseHelper(this, "BookStore.db", 2)
         createDatabase.setOnClickListener {
-            myDatabaseHelper.writableDatabase
+            dbHelper.writableDatabase
         }
 
-        // TODO CRUD 操作
+
+        val addData: Button = findViewById(R.id.addData)
+        val deleteData: Button = findViewById(R.id.deleteData)
+        val updateData: Button = findViewById(R.id.updateData)
+        val queryData: Button = findViewById(R.id.queryData)
+
+        // 添加数据
+        addData.setOnClickListener {
+            val db = dbHelper.writableDatabase
+
+            var book1 = ContentValues().apply {
+                put("name", "布布宝历险记")
+                put("author", "Walterxiong")
+                put("pages", 555)
+                put("price", 19.99)
+            }
+            db.insert("Book", null, book1)
+
+            var book2 = ContentValues().apply {
+                put("name", "The Da Vinci Code")
+                put("author", "Dan Brown")
+                put("pages", 454)
+                put("price", 16.66)
+            }
+            db.insert("Book", null, book2)
+        }
+
+        // 删除数据
+        deleteData.setOnClickListener {
+            val db = dbHelper.writableDatabase
+            db.delete("Book", "name = ?", arrayOf("The Da Vinci Code"))
+            Toast.makeText(this, "删除成功", Toast.LENGTH_SHORT).show()
+        }
+
+        // 更新数据
+        updateData.setOnClickListener {
+            val db = dbHelper.writableDatabase
+            val value = ContentValues()
+            value.put("price", 10.99)
+            db.update("Book", value, "name = ?", arrayOf("The Da Vinci Code"))
+            Toast.makeText(this, "更新成功", Toast.LENGTH_SHORT).show()
+        }
+
+        queryData.setOnClickListener {
+            val db = dbHelper.writableDatabase
+            var cursor = db.query("Book", null, null, null, null, null, null)
+            if (cursor.moveToFirst()) {
+                do {
+                    // 遍历Cursor对象，取出数据并打印
+                    val name = cursor.getString(cursor.getColumnIndexOrThrow("name"))
+                    val author = cursor.getString(cursor.getColumnIndexOrThrow("author"))
+                    val pages = cursor.getInt(cursor.getColumnIndexOrThrow("pages"))
+                    val price = cursor.getDouble(cursor.getColumnIndexOrThrow("price"))
+                    Log.d("MainActivity", "book name is $name")
+                    Log.d("MainActivity", "book author is $author")
+                    Log.d("MainActivity", "book pages is $pages")
+                    Log.d("MainActivity", "book price is $price")
+                } while (cursor.moveToNext())
+            }
+            cursor.close()
+
+            Toast.makeText(this, "查询成功", Toast.LENGTH_SHORT).show()
+        }
     }
 }
