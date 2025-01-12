911c9fd72681a4316eca4b64a50e90f5fe235f99
WalterXiong <xiongjun-ol@outlook.com>
2025-01-12 21:06:51 +0800

    新增-- RetrofitTest 1、使用协程优化回调写法

--- 11_Network/RetrofitTest/app/src/main/java/com/androidlearning/retrofittest/MainActivity.kt
+++ 11_Network/RetrofitTest/app/src/main/java/com/androidlearning/retrofittest/MainActivity.kt
@@ -8,12 +8,16 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.androidlearning.retrofittest.retrofit_best_practice.ServiceCreator
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.create
+import kotlin.coroutines.resume
+import kotlin.coroutines.resumeWithException
+import kotlin.coroutines.suspendCoroutine
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -52,6 +56,53 @@ class MainActivity : AppCompatActivity() {
                     p1.printStackTrace()
                 }
             })
+
+            val appService1 = ServiceCreator.create<AppService>()
+            appService1.getAppData().enqueue(object : Callback<List<App>> {
+                override fun onResponse(
+                    p0: Call<List<App>?>, p1: Response<List<App>?>
+                ) {
+                    TODO("Not yet implemented")
+                }
+
+                override fun onFailure(
+                    p0: Call<List<App>?>, p1: Throwable
+                ) {
+                    TODO("Not yet implemented")
+                }
+
+            })
         }
+    }
+}
+
+suspend fun getAppData() {
+
+    try {
+        val appList = ServiceCreator.create<AppService>().getAppData().await()
+        // 处理
+    } catch (e: Exception) {
+        e.printStackTrace()
+    }
+}
+
+suspend fun <T> Call<T>.await(): T {
+
+    return suspendCoroutine { continuation ->
+
+        enqueue(object : Callback<T> {
+
+            override fun onResponse(p0: Call<T?>, response: Response<T?>) {
+                val body = response.body()
+                if (body != null) continuation.resume(body)
+                else continuation.resumeWithException(
+                    RuntimeException("response body is null")
+                )
+            }
+
+            override fun onFailure(p0: Call<T?>, t: Throwable) {
+                continuation.resumeWithException(t)
+            }
+        })
     }
 }
