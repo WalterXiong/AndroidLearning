1dc02a8da3b7d52af072c98fad706f74e39656db
WalterXiong <xiongjun-ol@outlook.com>
2025-01-14 21:44:07 +0800

    新增-- MaterialTest

---
+++ 12_MaterialDesign/MaterialTest/app/src/main/java/com/androidlearning/materialtest/MainActivity.kt
@@ -0,0 +1,54 @@
+package com.androidlearning.materialtest
+
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.drawerlayout.widget.DrawerLayout
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var drawerLayout: DrawerLayout
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        drawerLayout = findViewById<DrawerLayout>(R.id.drawerLayout)
+
+        setSupportActionBar(findViewById<Toolbar>(R.id.toolbar))
+        supportActionBar?.let {
+            it.setDisplayHomeAsUpEnabled(true)
+            it.setHomeAsUpIndicator(R.drawable.ic_menu)
+        }
+
+    }
+
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.toolbar, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> drawerLayout.openDrawer(GravityCompat.START)
+            R.id.backup -> Toast.makeText(this, "备份", Toast.LENGTH_SHORT).show()
+            R.id.delete -> Toast.makeText(this, "删除", Toast.LENGTH_SHORT).show()
+            R.id.settings -> Toast.makeText(this, "设置", Toast.LENGTH_SHORT).show()
+        }
+        return true
+    }
+}
