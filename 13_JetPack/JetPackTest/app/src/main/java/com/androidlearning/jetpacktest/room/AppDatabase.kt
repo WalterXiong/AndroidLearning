36f5128d4bc6d524d10b0b8eed5926dd61ca3def
WalterXiong <xiongjun-ol@outlook.com>
2025-01-23 22:22:50 +0800

    新增-- JetPackTest (Room--数据库升级)

--- 13_JetPack/JetPackTest/app/src/main/java/com/androidlearning/jetpacktest/room/AppDatabase.kt
+++ 13_JetPack/JetPackTest/app/src/main/java/com/androidlearning/jetpacktest/room/AppDatabase.kt
@@ -4,14 +4,40 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
 import com.androidlearning.jetpacktest.viewmodeltest.User
 
-@Database(version = 1, entities = [User::class])
+// @Database(version = 1, entities = [User::class])
+/**
+ * 数据库升级
+ */
+//@Database(version = 2, entities = [User::class, Book::class])
+@Database(version = 3, entities = [User::class, Book::class])
 abstract class AppDatabase : RoomDatabase() {
 
     abstract fun userDao(): UserDao
 
+    abstract fun bookDao(): BookDao
+
     companion object {
+
+        // 定义一个新增表，升级变量
+        val MIGRATION_1_2 = object : Migration(1, 2) {
+            override fun migrate(db: SupportSQLiteDatabase) {
+                db.execSQL(
+                    "create table Book (id integer primary key autoincrement not null, name text not null, pages integer not null ) "
+                )
+            }
+        }
+
+        val MIGRATION_2_3 = object : Migration(2, 3) {
+            override fun migrate(db: SupportSQLiteDatabase) {
+                db.execSQL(
+                    "alter table Book add column author text not null default 'unknown'"
+                )
+            }
+        }
 
         private var instance: AppDatabase? = null
 
@@ -22,11 +48,13 @@ abstract class AppDatabase : RoomDatabase() {
             }
             return Room.databaseBuilder(
                 context.applicationContext,
-                AppDatabase::class.java,
-                "app_database"
-            ).build().apply {
-                instance = this
-            }
+                AppDatabase::class.java, "app_database"
+            )
+                // 增加数据库升级逻辑
+                .addMigrations(MIGRATION_1_2, MIGRATION_2_3)
+                .build().apply {
+                    instance = this
+                }
         }
     }
 }
---
+++ 13_JetPack/JetPackTest/app/src/main/java/com/androidlearning/jetpacktest/room/Book.kt
@@ -0,0 +1,11 @@
+package com.androidlearning.jetpacktest.room
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+class Book(var name: String, var pages: Int, var author: String) {
+
+    @PrimaryKey(autoGenerate = true)
+    var id: Long = 0
+}
