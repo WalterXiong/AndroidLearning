708f11def5d0a4c52c5e0117b16257f5775f2b17
WalterXiong <xiongjun-ol@outlook.com>
2025-02-07 12:43:24 +0800

    新增-- NetworkTest JSON 解析，异步回调

--- 11_Network/NetworkTest/app/src/main/java/com/androidlearning/networktest/callback/HttpUtil.kt
+++ 11_Network/NetworkTest/app/src/main/java/com/androidlearning/networktest/callback/HttpUtil.kt
@@ -3,14 +3,13 @@ package com.androidlearning.networktest.callback
 import okhttp3.Callback
 import okhttp3.OkHttpClient
 import okhttp3.Request
-import java.io.BufferedReader
-import java.io.InputStreamReader
-import java.net.HttpURLConnection
-import java.net.URL
 import kotlin.concurrent.thread
 
 object HttpUtil {
 
+    /**
+     * 使用 OKHttp 自带的回调函数
+     */
     fun sendHttpRequest(address: String, callback: Callback) {
         thread {
             try {
